b8b49a444b8c10f25fa55cfc7b7f429c5ca69057
Stephen Becker IV <sbeckeriv+git@gmail.com>
2011-08-05 07:44:21 -0700

    unsure where i left off

--- examples/becker2/shot.js
+++ examples/becker2/shot.js
@@ -1,4 +1,3 @@
-
 var currSelected   = null; 
 var browserTabs    = new Array();
 var shotQueue = new Array();
@@ -21,14 +20,17 @@ function selectBrowser(browserRef) {
 
 function canvasShot(browserRef, thumbImageRef) { 
   try { 
-    selectBrowser(browserRef);
-    src=camera.full_snapshot(browserRef,$(browserRef.contentDocument).width(),$(browserRef.contentDocument).height(),console);
-    console.log("#######"+$(browserRef).attr("becksrc"));
 
-    sendImage($(browserRef).attr("becksrc"),src);
+    selectBrowser(browserRef);
+    browserRef.contentWindow;
+    var src=camera.full_snapshot(browserRef,$(browserRef.contentDocument).width(),$(browserRef.contentDocument).height(),console);
+    console.log("#######"+$(browserRef).attr("becksrc"));
+    var becksrc = $(browserRef).attr("becksrc");
     $(browserRef).remove();
+    sendImage(becksrc,src);
     console.log($("iframe").size())
   } catch (i) { 
+    console.log("canvas shot error");
     console.log(i);
     console.log(i.message); 
     console.log(i.lineNumber); 
@@ -36,35 +38,79 @@ function canvasShot(browserRef, thumbImageRef) {
 } 
 
 function sendImage(url,data){
-  console.log(window.ssid)
-    webSocket.send( window.ssid+"||||thunbnail||||"+url+"||||"+data);
+  console.log("sending image (fax noise):"+url)
+                                           console.log(window.ssid)
+                                           webSocket.send( window.ssid+"||||thunbnail||||"+url+"||||"+data);
 };
-
+document.getElementById("browserContentArea").addEventListener("DOMNodeInserted", function(e){ 
+  console.log(e.target.tagName)
+  if(e.target.tagName==="IFRAME"){
+    currentIframeElementId = currentIframeElementId  = e.target.getAttribute("id");
+    var iframeElement = e.target;
+  alert(iframeElement);
+    var pm = require('web-content').ProgressMonitor();
+    pm.on('title-change', function(title) {
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent("ChromelessDOMTitleChange", true, false);
+      evt.title = title;
+      iframeElement.dispatchEvent(evt);
+    });
+    pm.on('progress', function(percentage) {
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent("ChromelessDOMProgress", true, false);
+      evt.percentage = percentage;
+      iframeElement.dispatchEvent(evt);
+    });
+    pm.on('load-start', function() {
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent("ChromelessDOMLoadStart", true, false);
+      iframeElement.dispatchEvent(evt);
+    });
+    pm.on('load-stop', function() {
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent("ChromelessDOMLoadStop", true, false);
+      iframeElement.dispatchEvent(evt);
+    });
+    pm.on('security-change', function(e) {
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent("ChromelessDOMSecurityChange", true, false);
+      evt.state = e.state; 
+      evt.strength = e.state; 
+      iframeElement.dispatchEvent(evt);
+    });
+    var currentIframeElementId = iframeElement.getAttribute("id");
+    document.getElementById("browserContentArea").addEventListener("DOMNodeRemoved", function(e){ 
+      if(e.target.getAttribute("id") == currentIframeElementId) { 
+        console.log("It's me!, detaching "); 
+        pm.detach();
+      } 
+    }, false);
+    pm.attach(iframeElement);
+  }
+},false);
 function newTab() { 
   var newBrowser = makeBrowser();
   var uniqueId   = Math.random();
   var browserUnique = "browser_unique_"+uniqueId;
-
   browserTabs.push(newBrowser);
   newBrowser.setAttribute("id",browserUnique);
-  newBrowser.addEventListener("ChromelessLoadProgress",function (e) { 
-    try{
-      console.log("ChromelssLoad "+$(document.getElementById(browserUnique)).attr("becksrc")+" "+e.percentage)
-    }catch(e){
-      console.log("error:Chromeprogress:"+e)
-    }
-
-    if(e.percentage>95){
-      syncTabs(browserUnique);
-      console.log("ChromelssLoad done")
-    }else{
-
-    }
+  newBrowser.addEventListener("ChromelessDOMProgress",function(e){
+    alert(e);
   },false);
-  console.log(browserUnique)
-  console.log($("iframe").size())
-    document.getElementById("browserContentArea").appendChild(newBrowser);
+  setTimeout(function(){
+    // snap a shot and remove iframe
+    if(document.getElementById(browserUnique)){
+      wc.stopload(document.getElementById(browserUnique));
+      syncTabs(browserUnique);    
+    }
+  },1000);
+
+  console.log(browserUnique);
+  console.log($("iframe").size());
+  document.getElementById("browserContentArea").appendChild(newBrowser);
   selectBrowser(newBrowser);
+
+  return browserUnique;
 } 
 
 function syncTabs(refBrowserId) { 
@@ -97,7 +143,8 @@ $(document).ready(function() {
   });
 });
 var watcher = setInterval(function(){
-  console.log($("iframe").size())
+  console.log($("iframe").size());
+  console.log(window.ssid);
   webSocket.send( window.ssid+"||||ping");
 
 },2000)
@@ -107,29 +154,28 @@ var webSocket = new WebSocket("ws://0.0.0.0:8080");
 webSocket.onopen = function(evt) {
 };
 webSocket.onmessage = function(evt) {
-  var message =evt.data;
+  console.log("Got message")
+    var message =evt.data;
   console.log("%%%%%%%"+message)
     if(message.match(/beck:/)){
-      window.newTab();
-      browserUniqueId = $(".browser_selected").attr("id");
-      var fragment = $.trim(message.replace("beck:",""));
-      $(".browser_selected").attr("src", url.guess(fragment));
-      $(".browser_selected").attr("becksrc", fragment);
+      var browserUnique = newTab();
+      console.log("got uniq")
 
-
+        var fragment = $.trim(message.replace("beck:",""));
+      $("#"+browserUnique).attr("src", url.guess(fragment));
+      $("#"+browserUnique).attr("becksrc", fragment);
+      console.log("message set:"+"#"+browserUnique);
     }else if(message.match(/ssid:/)){
-      console.log("__________sid"+message)
-
-
+      console.log("__________sid:"+message)
         window.ssid= $.trim(message.replace("ssid:",""));
     }else{
-      //alert(message);
+      console.log("Unknown message");
+      console.log(message);
     }
 
 };
 webSocket.onclose = function(evt) {
-  console.log(evt)
-    webSocket = new WebSocket("ws://0.0.0.0:8080");
+  webSocket = new WebSocket("ws://0.0.0.0:8080");
 
 };
 
